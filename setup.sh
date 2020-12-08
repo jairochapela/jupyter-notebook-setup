40413d07fa908e6ecd8719d3761788fef090aaa1
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-12-08 12:38:01 +0100

    Matplotlib y requerimientos

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python3 -m venv .env || exit 1
 . .env/bin/activate
-pip install -r requeriments.txt
-jupyter notebook
+pip install --no-cache-dir -r requeriments.txt
+python3 -m jupyter notebook
 deactivate
